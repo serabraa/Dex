93c52b8bc2211970ddff567dda50b7dd1c5bed82
serabraa <sergey.abraham7@gmail.com>
2025-12-02 17:49:38 +0400

    show stored images

--- Dex/PokemonDetail.swift
+++ Dex/PokemonDetail.swift
@@ -22,15 +22,24 @@ struct PokemonDetail: View {
                     .resizable()
                     .scaledToFit()
                     .shadow(color: .black, radius: 6)
-                AsyncImage(url: showShiny ? pokemon.shinyURL : pokemon.spriteURL){image in
-                    image
+                if pokemon.sprite == nil || pokemon.shiny == nil {
+                    AsyncImage(url: showShiny ? pokemon.shinyURL : pokemon.spriteURL){image in
+                        image
+                            .interpolation(.none)
+                            .resizable()
+                            .scaledToFit()
+                            .padding(.top,50)
+                            .shadow(color: .black, radius: 6)
+                    } placeholder:{
+                        ProgressView()
+                    }
+                } else {
+                    (showShiny ? pokemon.shinyImage : pokemon.spriteImage)
                         .interpolation(.none)
                         .resizable()
                         .scaledToFit()
                         .padding(.top,50)
                         .shadow(color: .black, radius: 6)
-                } placeholder:{
-                    ProgressView()
                 }
             }
             HStack{
