9e2d2f5767b18f70276c484de557eb3656539ed6
serabraa <sergey.abraham7@gmail.com>
2025-12-03 17:03:05 +0400

    add widget exntension

---
+++ DexWidget/DexWidget.swift
@@ -0,0 +1,93 @@
+//
+//  DexWidget.swift
+//  DexWidget
+//
+//  Created by Sergey on 02.12.25.
+//
+
+import WidgetKit
+import SwiftUI
+
+struct Provider: TimelineProvider {
+    func placeholder(in context: Context) -> SimpleEntry {
+        SimpleEntry.placeholder
+    }
+
+    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
+        let entry = SimpleEntry.placeholder
+        completion(entry)
+    }
+
+    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+        var entries: [SimpleEntry] = []
+
+        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
+        let currentDate = Date()
+        for hourOffset in 0 ..< 5 {
+            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
+            let entry = SimpleEntry.placeholder
+            entries.append(entry)
+        }
+
+        let timeline = Timeline(entries: entries, policy: .atEnd)
+        completion(timeline)
+    }
+}
+
+struct SimpleEntry: TimelineEntry {
+    let date: Date
+    let name: String
+    let types: [String]
+    let sprite: Image
+    
+    static var placeholder: SimpleEntry{
+        SimpleEntry(date: .now,
+                    name: "bulbasaur",
+                    types: ["grass","poison"],
+                    sprite: Image(.bulbasaur))
+    }
+    
+    static var anotherPlaceHolder: SimpleEntry
+    {
+        SimpleEntry(date: .now,
+                    name: "mew",
+                    types: ["psychic"],
+                    sprite: Image(.mew))
+    }
+}
+
+struct DexWidgetEntryView : View {
+    var entry: Provider.Entry
+
+    var body: some View {
+        VStack {
+            entry.sprite
+        }
+    }
+}
+
+struct DexWidget: Widget {
+    let kind: String = "DexWidget"
+
+    var body: some WidgetConfiguration {
+        StaticConfiguration(kind: kind, provider: Provider()) { entry in
+            if #available(iOS 17.0, *) {
+                DexWidgetEntryView(entry: entry)
+                    .containerBackground(.fill.tertiary, for: .widget)
+            } else {
+                DexWidgetEntryView(entry: entry)
+                    .padding()
+                    .background()
+            }
+        }
+        .configurationDisplayName("My Widget")
+        .description("This is an example widget.")
+    }
+}
+
+#Preview(as: .systemSmall) {
+    DexWidget()
+} timeline: {
+    SimpleEntry.placeholder
+    SimpleEntry.anotherPlaceHolder
+}
---
+++ DexWidget/DexWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  DexWidgetBundle.swift
+//  DexWidget
+//
+//  Created by Sergey on 02.12.25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct DexWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        DexWidget()
+    }
+}
